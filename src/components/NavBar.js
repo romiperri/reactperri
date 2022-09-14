c129d579ebe571a77ceb59e4302d16fdfcb91368
romiperri <romyperriluna@gmail.com>
2022-09-13 22:43:09 -0300

    menuJsx

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import './navBar.css';
+
+function NavBar() {
+    return (
+
+        <div className="navBar">
+            <h1>Tienda Eco</h1>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contacts</a></li>
+            </ul>
+        </div>
+
+    );
+}
+
+export default NavBar; 
