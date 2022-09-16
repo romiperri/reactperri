fd3f3c69cc3b876496c4ca5ced6a5458c58cef39
romiperri <romyperriluna@gmail.com>
2022-09-16 20:18:54 -0300

    changes

---
+++ src/components/Productos/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import './Items.css'
+
+function ItemListContainer(props) {
+    return (
+        <div>
+            <h2>Productos de la tienda</h2>
+        </div>
+    );
+}
+
+export default ItemListContainer; 
