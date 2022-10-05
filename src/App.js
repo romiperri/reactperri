59f3e7a52e1c631b89bf1b1699e7ddeac9dd50cf
romiperri <romyperriluna@gmail.com>
2022-10-04 21:47:57 -0300

    Sincronizar Counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import './App.css';
-import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
-import NavBar from './components/NavBar';
-import ItemListContainer from './components/Productos/ItemListContainer';
+import "./App.css";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import NavBar from "./components/NavBar";
+import ItemListContainer from "./components/Productos/ItemListContainer";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path="/" element={<ItemListContainer greeting="Tienda online de libros" />} />
-        <Route path='/category/:cat' element={<ItemListContainer greeting="Tienda online de libros" />} />
+        <Route
+          path="/"
+          element={<ItemListContainer greeting="Tienda online de libros" />}
+        />
+        <Route
+          path="/category/:cat"
+          element={<ItemListContainer greeting="Tienda online de libros" />}
+        />
         <Route path="/detail/:id" element={<ItemDetailContainer />} />
+        <Route path="/cart" />
       </Routes>
-
     </BrowserRouter>
-
   );
 }
 
